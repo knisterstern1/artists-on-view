c21a37a6ca9b7a603b55d29ac443cebe826b835f
Christian Steiner <christian.steiner@unibas.ch>
2025-07-11 12:36:26 +0200

    testing webcomponent on aws

---
+++ artists.js
@@ -0,0 +1,19 @@
+import {html, css, LitElement} from 'https://esm.run/lit';
+
+export class SimpleGreeting extends LitElement {
+  static styles = css`p { color: blue }`;
+
+  static properties = {
+    name: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.name = 'Somebody';
+  }
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+customElements.define('artist-on-view', SimpleGreeting);
