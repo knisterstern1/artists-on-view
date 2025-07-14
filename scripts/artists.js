7f8df90b722c5f163eff625f5eb632f58167e52d
Christian Steiner <christian.steiner@unibas.ch>
2025-07-14 16:56:07 +0200

    filter added

--- scripts/artists.js
+++ scripts/artists.js
@@ -1,35 +1,188 @@
 import {html, css, LitElement} from 'https://esm.run/lit';
 import './collapsibleItem.js'
 
+const BUILDINGS = {
+   HB: 0,
+   NB: 1,
+   GW: 2
+}
+
+const MODI = {
+   ARTIST: 0,
+   LOCATION: 1
+}
+
+const MODUS = [ { type: MODI.ARTIST, placeholder: "Künstler:innen", label: "artist" },
+                { type: MODI.LOCATION, placeholder: "Standorte", label: "location"  } ]
+
 export class ArtistsOnView extends LitElement {
-  //static styles = css`p { color: blue }`;
+  static styles = css`
+  div.navi { display: inline; }
+  div.footer { display: inline-flex; }
+  label, div.status { font-size: 11px;}
+  div.status { margin-left: 50px;}
+  button { border: none; 
+      font-size: 16px;
+      float: right;
+      margin-top: 15px;
+  }
+  .hidden { display: none; }
+  input#nameFilter {  width: 80%; 
+  background-image: url('/styles/searchicon.png');
+  background-position: 10px 12px;
+  background-repeat: no-repeat;
+  font-size: 16px; 
+  padding: 12px 20px 12px 40px; 
+  border: 1px solid #ddd; 
+  margin-bottom: 12px }`;
 
   static properties = {
-    data: {type: Object}
+    data: {type: Object},
+    modus: {type: Object}
   };
 
   constructor() {
     super();
-    this.data = {}
+    this.data = {};
+    this.modus = MODUS[0]
     this.getData();
   }
-  getData() {
+  getData(filterKey) {
       fetch('artists.json').then(response=>{
          if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
          }
          return response.json();  
       }).then(data =>{
+         if (filterKey && filterKey != '' && this.modus.type == MODI.ARTIST) {
+            data.artists = data.artists.filter(item =>item.Name.toUpperCase().startsWith(filterKey) || item.Vorname.toUpperCase().startsWith(filterKey))
+         }
+         data.locations = this.__createLocations(data.artists, filterKey, (this.modus.type == MODI.LOCATION));
          this.data = data;
       }).catch(error => console.error('Failed to fetch data:', error));
+  }
+  __createLocations(artists, filterKey, isLocationModus) {
+      let locations = {};
+      if (isLocationModus && filterKey && filterKey != '') {
+          artists.forEach( artist =>{
+            artist.Objekte.forEach(obj =>{
+               if (obj.Location.toUpperCase().startsWith(filterKey)) {
+                  if (!(obj.Location in locations)) {
+                     locations[obj.Location] = [];
+                  }
+                  if (locations[obj.Location].filter(currentArtist =>currentArtist.ID == artist.ID).length == 0){
+                     locations[obj.Location].push(this.__filterArtist(artist, obj.Location))
+                  }
+               }
+            });
+         });
+      } else {
+         artists.forEach( artist =>{
+            artist.Objekte.forEach(obj =>{
+               if (!(obj.Location in locations)) {
+                  locations[obj.Location] = [];
+               }
+               if (locations[obj.Location].filter(currentArtist =>currentArtist.ID == artist.ID).length == 0){
+                  locations[obj.Location].push(this.__filterArtist(artist, obj.Location))
+               }
+            });
+         });
+      }
+      let locationArray = [];
+      for (const [key, value] of Object.entries(locations)) {
+         locationArray.push({ location: key, artists: value, sortLnu: this.__getSortLnu(key) })
+      }
+      return locationArray.sort((a, b) => (a.sortLnu < b.sortLnu) ? -1 : 1);
+  }
+  __getSortLnu(locationStr) {
+      const locArray = locationStr.split('.');
+      if (locArray.length < 2) {
+         return BUILDINGS[locArray[0]]
+      }
+      const second = (isNaN(locArray[1])) ? '0' : locArray[1]
+      const third = (locArray.length < 3 || isNaN(locArray[1])) ? '0' : locArray[2]
+      return BUILDINGS[locArray[0]] + second + third
+  }
+  __filterArtist(artist, objLocation) {
+      let clonedArtist = structuredClone(artist)
+      clonedArtist.Objekte = clonedArtist.Objekte.filter(obj =>obj.Location == objLocation) 
+      return clonedArtist
+  }
+
+  __filterNames(e) {
+     if (e.target) {
+         const targetKey = '#' + e.target.id;
+         const buttonSelector = (e.target.value != '') ? '*[data-target=\"' + targetKey + '\"].hidden' : '*[data-target=\"' + targetKey + '\"]:not(hidden)';
+         const button = this.renderRoot?.querySelector(buttonSelector)
+         if (button) {
+            button.classList.toggle('hidden')
+         }
+         this.getData(e.target.value.toUpperCase())
+     }
+  }
+  __cancel(e) {
+      if (e.target && e.target.dataset.target){
+         this.__resetInput(e.target)
+      }
+      this.getData();
+  }
+  __resetInput(button) {
+      button.classList.add('hidden')
+      const input = this.renderRoot?.querySelector(button.dataset.target)
+      if (input) {
+         input.value = '';
+      }
+  }
+  __changeModus(e) {
+     this.modus = MODUS[e.target.value];
+     const button = this.renderRoot?.querySelector(e.target.dataset.target)
+     if (button) {
+        this.__resetInput(button);
+     }
+  }
+  __renderContent() {
+      return (this.modus.type == MODI.ARTIST) 
+      ?   html` ${this.data.artists.map(artist => html`<collapsible-item .artist="${artist}"></collapsible-item>`)}`
+      :   html` ${this.data.locations.map(location => html`<collapsible-item .location="${location}"></collapsible-item>`)}`
+  }
+  __lastUpdate() {
+     if (this.data.endDate) {
+        const dateArray = this.data.endDate.split('_')
+        const options = {
+           weekday: "long",
+           year: "numeric",
+           month: "long",
+           day: "numeric",
+         };
+        let date = new Date(dateArray[0])
+        if (dateArray.length > 1) {
+           const timeStr = dateArray[1].replaceAll('-',':')
+           date = new Date(dateArray[0] + 'T' + timeStr)
+        }
+        return date.toLocaleDateString("de-DE", options) + ', ' + date.toLocaleTimeString("de-DE");
+     }
+     return '????'
   }
 
   render() {
     if (this.data.status && this.data.status == 'success') {
       return html`
-      <p>
-         ${this.data.artists.map(i => html`<collapsible-item .artist="${i}"></collapsible-item>`)}
-      </p>`;
+      <div class="navi">
+         <input type="text" id="nameFilter" @keyup=${this.__filterNames} placeholder=${this.modus.placeholder}>
+         <button id="cancelButton" class="hidden" data-target="#nameFilter" @click=${this.__cancel}>&#10006;</button><br>
+         <div class="footer">
+         ${MODUS.map(modus => html`<input data-target="#cancelButton" 
+                                          type="radio" 
+                                          @click=${this.__changeModus} 
+                                          id=${modus.label} 
+                                          name="modus" 
+                                          value=${modus.type} 
+                                          ?checked=${this.modus.type == modus.type}> 
+                                          <label for=${modus.label}>${modus.placeholder}</label>`)}
+            <div class="status">Letzte &Auml;nderung: ${this.__lastUpdate()}</div>
+         </div>
+      </div>
+      <p>${this.__renderContent()}</p>`;
     } 
   }
 }
--- scripts/collapsibleItem.js
+++ scripts/collapsibleItem.js
@@ -2,44 +2,73 @@ import {html, css, LitElement} from 'https://esm.run/lit';
 
 export class CollapsibleItem extends LitElement {
   static styles = css`
+  .accordion { 
+     cursor: pointer;
+     transition: 0.4s;}
+  .accordion:hover { background: #f9f9f9 ; }
+  .accordion-header:after {
+     content: '\\02795'; 
+     font-size: 13px;
+      color: #777;
+     float: right;
+     margin-left: 5px;
+   }
+   .active:after {
+      content: '\\2796';
+   }
   td.location { padding-left: 20px; }
   .hidden { display: none }`;
 
   static properties = {
-    artist: {type: Object}
+    artist: {type: Object},
+    location: {type: Object}
   };
 
   constructor() {
     super();
     this.artist = null;
+    this.location = null;
   }
 
-  artistInfo(){
-      if (this.artist.Lebensdaten) {
-         return this.artist.Vorname + " " + this.artist.Name + " (" + this.artist.Lebensdaten + ")"
+  artistInfo(artist){
+     if (artist.Lebensdaten) {
+         return artist.Vorname + " " + artist.Name + " (" + artist.Lebensdaten + ")"
       }
-      return this.artist.Vorname + " " + this.artist.Name
+      return artist.Vorname + " " + artist.Name
   }
 
   __itemClicked(e) {
       const item = this.renderRoot?.querySelector('#hiddenItem')
-      if (item) {
+      const header = this.renderRoot?.querySelector('#header')
+      if (item && header) {
          item.classList.toggle('hidden')
+         header.classList.toggle('active')
       }
   }
 
   render() {
     if (this.artist) {
       return html`
-      <div @click=${this.__itemClicked}>
-         <h3>${this.artistInfo()}</h3>
+      <div class="accordion" @click=${this.__itemClicked}>
+         <h3 id="header" class="accordion-header">${this.artistInfo(this.artist)}</h3>
          <div id="hiddenItem" class="hidden">
             <table> 
             ${this.artist.Objekte.map(obj => html`<tr><td>&quot;${obj.Titles}&quot;</td><td class="location">${obj.Location}</td></tr>`)}
             </table>
          </div>
       </div>`;
-    } 
+    } else if (this.location) {
+      return html`
+      <div class="accordion" @click=${this.__itemClicked}>
+         <h3 id="header" class="accordion-header">${this.location.location}</h3>
+         <div id="hiddenItem" class="hidden">
+           ${this.location.artists.map(artist =>
+              html`<h4>${this.artistInfo(artist)}</h4>
+                   ${artist.Objekte.map(obj => html`<div>&quot;${obj.Titles}&quot;</div>`)}
+                  `)}     
+         </div>
+      </div>`;
+    }
   }
 }
 customElements.define('collapsible-item', CollapsibleItem);
